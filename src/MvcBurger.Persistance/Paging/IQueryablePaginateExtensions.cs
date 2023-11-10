4142ae66852c1b2aa8f4f525bd0054cb0a2223f6
CanberkTimurlenk <timurlenk.canberk@gmail.com>
2023-11-10 19:38:46 +0300

    Create Branch

---
+++ src/MvcBurger.Persistance/Paging/IQueryablePaginateExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MvcBurger.Persistance.Paging
+{
+    public static class IQueryablePaginateExtensions
+    {
+        public static async Task<PagedList<T>> ToPaginatedListAsync<T>(
+            this IQueryable<T> source,
+            int index,
+            int size
+            )
+        {
+            int count = await source.CountAsync().ConfigureAwait(false);
+
+            List<T> items = await source.Skip(index * size).Take(size).ToListAsync().ConfigureAwait(false);
+
+            PagedList<T> list = new()
+            {
+                Index = index,
+                Count = count,
+                Items = items,
+                Size = size,
+                Pages = (int)Math.Ceiling(count / (double)size)
+            };
+            return list;
+
+        }
+    }
+}
---
+++ src/MvcBurger.Persistance/Repositories/WriteRepository.cs
@@ -0,0 +1,55 @@
+﻿using MvcBurger.Application.Repositories;
+using MvcBurger.Domain.Entities.Common;
+using MvcBurger.Persistance.Contexts;
+
+namespace MvcBurger.Persistance.Repositories
+{
+    public class WriteRepository<TEntity> : IWriteRepository<TEntity>
+        where TEntity : BaseEntity, new()
+    {
+        //
+        private readonly BurgerDbContext _context;
+
+        public WriteRepository(BurgerDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> AddRangeAsync(IEnumerable<TEntity> entities)
+        {
+            foreach (var entity in entities)
+                entity.CreatedDate = DateTime.UtcNow;
+
+            await _context.AddRangeAsync(entities);
+            return await _context.SaveChangesAsync();
+        }
+
+        public async Task<bool> AddAsync(TEntity entity)
+        {
+            entity.CreatedDate = DateTime.UtcNow;
+            await _context.AddAsync(entity);
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        public async Task<bool> RemoveRangeAsync(IEnumerable<TEntity> entities)
+        {
+            _context.Set<TEntity>().RemoveRange(entities);
+
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        public async Task<bool> RemoveAsync(string id)
+        {
+            var entity = await _context.Set<TEntity>().FindAsync(Guid.Parse(id));
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        public TEntity UpdateAsync(TEntity entity)
+        {
+            entity.UpdatedDate = DateTime.UtcNow;
+            _context.Set<TEntity>().Update(entity);
+            _context.SaveChangesAsync();
+            return entity;
+        }
+    }
+}
