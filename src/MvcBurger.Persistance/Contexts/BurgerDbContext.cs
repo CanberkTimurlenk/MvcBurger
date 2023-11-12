f31bc4b4921e70aa0695883d677b4ca8da1c8d7f
CanberkTimurlenk <timurlenk.canberk@gmail.com>
2023-11-12 19:38:33 +0300

    Modify Context

--- src/MvcBurger.Persistance/Contexts/BurgerDbContext.cs
+++ src/MvcBurger.Persistance/Contexts/BurgerDbContext.cs
@@ -6,9 +6,8 @@ namespace MvcBurger.Persistance.Contexts
     public class BurgerDbContext : DbContext
     {
 
-
         public DbSet<Menu> Menus { get; set; }
-        public DbSet<MenuOrder> MenuOrders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
         public DbSet<Order> Orders { get; set; }
         public DbSet<Drink> Drinks { get; set; }
         public DbSet<Sauce> Sauces { get; set; }
@@ -20,58 +19,10 @@ namespace MvcBurger.Persistance.Contexts
         {
             optionsBuilder.UseSqlServer("Server=127.0.0.1,11433;Database=HamburgerDb;User ID=SA;Password=Ab12345678;TrustServerCertificate=True");
         }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<MenuOrder>()
-                .HasKey(mo => new { mo.MenuOrderId });
-
-            modelBuilder.Entity<MenuOrder>()
-                .HasOne(mo => mo.Menu)
-                .WithMany(m => m.MenuOrder)
-                .HasForeignKey(mo => mo.MenuId);
-
-            modelBuilder.Entity<MenuOrder>()
-                .HasOne(mo => mo.Order)
-                .WithMany(o => o.MenuOrder)
-                .HasForeignKey(mo => mo.OrderId);
-
-            modelBuilder.Entity<MenuOrder>()
-                .HasOne(mo => mo.Drink)
-                .WithMany(d => d.MenuOrder)
-                .HasForeignKey(mo => mo.DrinkId);
-
-
-            modelBuilder.Entity<MenuOrderExtraIngredient>()
-                .HasKey(moei => new { moei.ExtraIngredientId, moei.MenuOrderId, });
-
-            modelBuilder.Entity<MenuOrderExtraIngredient>()
-                .HasOne(moei => moei.MenuOrder)
-                .WithMany(mo => mo.MenuOrderExtraIngredient)
-                .HasForeignKey(moei => moei.MenuOrderId);
-
-
-            modelBuilder.Entity<MenuOrderExtraIngredient>()
-                .HasOne(moei => moei.ExtraIngredient)
-                .WithMany(ei => ei.MenuOrderExtraIngredient)
-                .HasForeignKey(moei => moei.ExtraIngredientId);
-
-
-            modelBuilder.Entity<SauceOrder>()
-                .HasKey(so => new { so.SauceId, so.OrderId });
-
-            modelBuilder.Entity<SauceOrder>()
-                .HasOne(so => so.Sauce)
-                .WithMany(s => s.SauceOrder)
-                .HasForeignKey(so => so.SauceId);
-
-            modelBuilder.Entity<SauceOrder>()
-                .HasOne(so => so.Order)
-                .WithMany(o => o.SauceOrder)
-                .HasForeignKey(so => so.OrderId);
-
-
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(BurgerDbContext).Assembly);
         }
-
-
     }
 }
