3d2869a451139f7d87efb04ec98507f424db1a8a
CanberkTimurlenk <timurlenk.canberk@gmail.com>
2023-11-12 19:59:13 +0300

    Add Drink Repository

---
+++ src/MvcBurger.Persistance/Repositories/DrinkRepository.cs
@@ -0,0 +1,13 @@
+﻿using MvcBurger.Application.Repositories;
+using MvcBurger.Domain.Entities;
+using MvcBurger.Persistance.Contexts;
+
+namespace MvcBurger.Persistance.Repositories
+{
+    public class DrinkRepository : EfBaseRepository<Drink>, IDrinkRepository
+    {
+        public DrinkRepository(BurgerDbContext context) : base(context)
+        {
+        }
+    }
+}
