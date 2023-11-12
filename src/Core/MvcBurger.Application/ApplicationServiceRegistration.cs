73b658273a745493ec9ad331faf862ab5babd92d
CanberkTimurlenk <timurlenk.canberk@gmail.com>
2023-11-12 19:48:53 +0300

    Add ApplicationServiceRegistration

---
+++ src/Core/MvcBurger.Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace MvcBurger.Application
+{
+    public static class ApplicationServiceRegistration
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+            //services.AddSubClassesOfType(Assembly.GetExecutingAssembly(), typeof(BaseBusinessRules));
+            
+
+            services.AddMediatR(configuration =>
+            {
+                configuration.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+
+                //configuration.AddOpenBehavior(typeof(RequestValidationBehavior<,>));
+                //configuration.AddOpenBehavior(typeof(TransactionScopeBehavior<,>));
+
+            });
+
+            return services;
+        }
+
+        /*
+        public static IServiceCollection AddSubClassesOfType(
+           this IServiceCollection services,
+           Assembly assembly,
+           Type type,
+           Func<IServiceCollection, Type, IServiceCollection>? addWithLifeCycle = null
+       )
+        {
+            var types = assembly.GetTypes().Where(t => t.IsSubclassOf(type) && type != t).ToList();
+            foreach (var item in types)
+                if (addWithLifeCycle == null)
+                    services.AddScoped(item);
+
+                else
+                    addWithLifeCycle(services, type);
+            return services;
+        }
+        */
+    }
+}
