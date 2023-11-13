d58e5eadc87899c5fb8913f20dde6a024a737cf6
batuhanbora <batuhanbora.alaftar@bilgeadamboost.com>
2023-11-13 21:27:06 +0300

    Add Views and Template

---
+++ src/MvcBurger.Presentation/MvcBurger.Web/wwwroot/js/loginregister.js
@@ -0,0 +1,9 @@
+﻿namespace MvcBurger.Web.wwwroot.js
+{
+    public class loginregister
+    {
+        $('.message a').click(function() {
+            $('form').animate({ height: "toggle", opacity: "toggle" }, "slow");
+        });
+    }
+}
