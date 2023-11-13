261a5ee6ed76ff9d0f3a0814463969737b3463ff
CanberkTimurlenk <timurlenk.canberk@gmail.com>
2023-11-13 13:05:55 +0300

    Add Dtos For User Create and Login

---
+++ src/Core/MvcBurger.Application/DTOs/User/Create/CreateUser.cs
@@ -0,0 +1,11 @@
+﻿namespace MvcBurger.Application.DTOs.User.Create
+{
+    public class CreateUser
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ src/Core/MvcBurger.Application/DTOs/User/Create/CreateUserResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace MvcBurger.Application.DTOs.User.Create
+{
+    public class CreateUserResponse
+    {
+        public bool Succeeded { get; set; }
+        public string Message { get; set; }
+    }
+}
