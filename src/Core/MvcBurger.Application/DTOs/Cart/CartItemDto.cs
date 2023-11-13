fc33ecf9d906fdd262f471c2def9a31dba59a20d
CanberkTimurlenk <timurlenk.canberk@gmail.com>
2023-11-13 22:20:31 +0300

    Add Amount to Cart

--- src/Core/MvcBurger.Application/DTOs/Cart/CartItemDto.cs
+++ src/Core/MvcBurger.Application/DTOs/Cart/CartItemDto.cs
@@ -1,5 +1,4 @@
-﻿using MvcBurger.Domain.Entities;
-using MvcBurger.Domain.Enums;
+﻿using MvcBurger.Domain.Enums;
 
 namespace MvcBurger.Application.DTOs.Cart
 {
@@ -7,8 +6,11 @@ namespace MvcBurger.Application.DTOs.Cart
     {
         public Guid Id { get; set; }
         public Size Size { get; set; }
+        public int Amount { get; set; }
         public MenuDto Menu { get; set; }
         public DrinkDto Drink { get; set; }
+
+
         public IEnumerable<ExtraIngredientDto> ExtraIngredients { get; set; }
 
     }
