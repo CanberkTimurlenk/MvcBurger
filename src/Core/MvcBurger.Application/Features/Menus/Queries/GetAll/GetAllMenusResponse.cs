ddbca3f4e8178c285a20d3960eb3e5d3380378c6
CanberkTimurlenk <timurlenk.canberk@gmail.com>
2023-11-13 21:07:39 +0300

    Update GetAllMenusResponse

--- src/Core/MvcBurger.Application/Features/Menus/Queries/GetAll/GetAllMenusResponse.cs
+++ src/Core/MvcBurger.Application/Features/Menus/Queries/GetAll/GetAllMenusResponse.cs
@@ -12,5 +12,6 @@
         public string Name { get; set; }
         public string Description { get; set; }
         public decimal Price { get; set; }
+        public string ImageUrl { get; set; }
     }
 }
