1d8464887e347afc793f9fb4a26dc3c9d5b6cf55
CanberkTimurlenk <timurlenk.canberk@gmail.com>
2023-11-13 13:10:38 +0300

    Add Login Query For User

---
+++ src/Core/MvcBurger.Application/Features/Users/Queries/Login/LoginAppUserQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using MediatR;
+using MvcBurger.Application.Contracts.Services;
+
+namespace MvcBurger.Application.Features.Users.Queries.Login
+{
+    public class LoginAppUserQueryHandler : IRequestHandler<LoginAppUserRequest, LoginAppUserResponse>
+    {
+
+        private readonly IAuthService _authService;
+
+        public LoginAppUserQueryHandler(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        public async Task<LoginAppUserResponse> Handle(LoginAppUserRequest request, CancellationToken cancellationToken)
+        {
+            var result = await _authService.LoginAsync(request.Email, request.Password);
+
+            return new LoginAppUserResponse { Succeeded = result };
+
+        }
+    }
+}
---
+++ src/Core/MvcBurger.Application/Features/Users/Queries/Login/LoginAppUserResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace MvcBurger.Application.Features.Users.Queries.Login
+{
+    public record LoginAppUserResponse
+    {
+        public bool Succeeded { get; set; }
+    }
+}
