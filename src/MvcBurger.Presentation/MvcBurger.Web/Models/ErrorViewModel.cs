f977971a01ff41182ec37c624fee3a1e3481ca68
batuhanbora <batuhanbora.alaftar@bilgeadamboost.com>
2023-11-13 21:23:54 +0300

    Add VMs

---
+++ src/MvcBurger.Presentation/MvcBurger.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MvcBurger.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
