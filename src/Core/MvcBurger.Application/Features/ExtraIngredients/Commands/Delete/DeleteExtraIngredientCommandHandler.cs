ddebf1e1b1fa1128c6a0076b1fb77da47e495abb
CanberkTimurlenk <timurlenk.canberk@gmail.com>
2023-11-14 22:55:00 +0300

    Add Delete & Update Features for ExtraIngredients

---
+++ src/Core/MvcBurger.Application/Features/ExtraIngredients/Commands/Delete/DeleteExtraIngredientCommandHandler.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using MediatR;
+using MvcBurger.Application.Contracts.Repositories.RepositoryManager;
+using MvcBurger.Application.Exceptions.NotFoundException;
+using MvcBurger.Domain.Entities;
+
+namespace MvcBurger.Application.Features.Orders.Commands.Cart.UpdateCartItem
+{
+    public class DeleteExtraIngredientCommandHandler : IRequestHandler<DeleteExtraIngredientRequest, DeleteExtraIngredientResponse>
+    {
+        private readonly IRepositoryManager _repositoryManager;
+        private readonly IMapper _mapper;
+
+        public DeleteExtraIngredientCommandHandler(IRepositoryManager repositoryManager, IMapper mapper)
+        {
+            _repositoryManager = repositoryManager;
+            _mapper = mapper;
+            // business rules if exists
+
+        }
+
+        public async Task<DeleteExtraIngredientResponse> Handle(DeleteExtraIngredientRequest request, CancellationToken cancellationToken)
+        {
+
+            await _repositoryManager.ExtraIngredient.RemoveByIdAsync(request.ExtraIngredientId);
+            await _repositoryManager.SaveAsync();
+
+            return new DeleteExtraIngredientResponse();
+
+        }
+    }
+
+}
+
+
+
+
