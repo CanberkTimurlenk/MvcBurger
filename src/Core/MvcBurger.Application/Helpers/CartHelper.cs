0f7a3c1266cad59cf931984fa29611ec7561acd1
CanberkTimurlenk <timurlenk.canberk@gmail.com>
2023-11-15 16:36:11 +0300

    Add GetTotalCartPrice

---
+++ src/Core/MvcBurger.Application/Helpers/CartHelper.cs
@@ -0,0 +1,21 @@
+﻿using MvcBurger.Application.DTOs.Cart;
+
+namespace MvcBurger.Application.Helpers
+{
+    public static class CartHelper
+    {
+        public static decimal GetTotalCartPrice(CartDto cartDto)
+        {
+            decimal total = 0;
+            cartDto.CartItems.ToList().ForEach(cartItem =>
+            {
+                total += cartItem.Menu.Price * cartItem.Amount;
+                cartItem.ExtraIngredients.ToList().ForEach(extraIngredient =>
+                {
+                    total += extraIngredient.Price * cartItem.Amount;
+                });
+            });
+            return total;
+        }
+    }
+}
