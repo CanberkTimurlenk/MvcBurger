e3a4db79ac5f25ec6f59d256140149937527d91e
Canberk TİMURLENK <canberk.timurlenk@gmail.com>
2023-11-23 12:25:48 +0300

    Remove Current Pagination Extensions

--- src/Core/MvcBurger.Application/RequestFeatures/RequestParameters.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MvcBurger.Application.RequestFeatures
-{
-    public class RequestParameters
-    {
-        public int PageNumber { get; set; }
-        public int PageSize { get; set; }
-    }
-}
